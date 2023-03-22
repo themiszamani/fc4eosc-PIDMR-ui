cf038e4d464b2e98cc21bd58eb0ec19818dc9fc0
Konstantinos Kagkelidis <kaggis@gmail.com>
2023-03-22 15:38:09 +0200

    PIDMR-26 Support and validate arxiv pids

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { useState } from 'react';
 import { Routes, Route, Link } from "react-router-dom"
-import {Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 // check if a pid is valid ark
-function isValidARK(pid){
+function isValidARK(pid) {
   const regex = /^ark:(?:\/\d{5,9})+\/[a-zA-Z\d]+(-[a-zA-Z\d]+)*$/
   return regex.test(pid);
+}
+
+// check if a pid is valid arxiv
+function isValidArxiv(pid) {
+  // regex for the modern schema
+  const regex = /^ar(X|x)iv:\d{2}(0[1-9]|1[0-2])\.\d{4,5}(v\d+)?$/
+  // regex for the old schema
+  const oldRegex = /^ar(X|x)iv:(astro-ph|cond-mat|gr-qc|hep-ex|hep-lat|hep-ph|hep-th|math-ph|nlin|nucl-ex|nucl-th|physics|quant-ph|math|CoRR|q-bio|q-fin|stat|eess|econ)(\.[A-Z][A-Z])?\/\d{2}(0[1-9]|1[0-2])\d+(v\d+)?$/
+  let result = regex.test(pid)
+  // try to match with old schema
+  if (!result) {
+    return oldRegex.test(pid)
+  }
+  return result
 }
 
 // Component to render the metaresolver page
@@ -16,7 +30,7 @@ function Main() {
   const [pid, setPid] = useState("")
   const [redirect, setRedirect] = useState(true)
   const [valid, setValid] = useState(false)
-  const [tooltip, setTooltip] = useState("Please enter a valid ark id")
+  const [tooltip, setTooltip] = useState("Please enter a valid pid (ark, arxiv)")
 
   function handleChange(event) {
     let pid = event.target.value.trim()
@@ -31,8 +45,21 @@ function Main() {
         setValid(false)
         setTooltip("Please enter a valid ARK pid (e.g. ark:/13030/tf5p30086k)")
       }
+    } else if (pid.toLowerCase().startsWith("arxiv:")) {
+      if (isValidArxiv(pid)) {
+        setValid(true)
+        setTooltip("this is a valid arxiv pid")
+      } else {
+        setValid(false)
+        setTooltip("Please enter a valid arxiv pid (e.g. arxiv:1512.00135)")
+      }
+    } else {
+      setValid(false)
+      setTooltip("Please enter a valid pid (ark, arxiv)")
     }
-    console.log(isValidARK(pid))
+
+
+
 
   }
 
@@ -41,12 +68,12 @@ function Main() {
     jump_url = jump_url + "?noredirect"
   }
 
-  
+
   let tipcolor = "grey"
   if (valid) {
     tipcolor = "green"
   }
- 
+
 
   return (
     <div className="page-center">
@@ -55,27 +82,27 @@ function Main() {
         {/* Intro icons and text */}
         <h1 className="text-large">🏷️ ➜ 📦</h1>
         <p>The FC4EOSC Metaresolver resolves individual handles from various providers</p>
-        
+
         {/* Pid input box */}
-        <input type="text" className="form-control" style={{ 'max-width': '600px' }} id="input-pid" 
-        value={pid} onChange={(event) => {handleChange(event)}} />
-         <em style={{color: tipcolor}}>{tooltip}</em>
+        <input type="text" className="form-control" style={{ 'max-width': '600px' }} id="input-pid"
+          value={pid} onChange={(event) => { handleChange(event) }} />
+        <em style={{ color: tipcolor }}>{tooltip}</em>
 
         {/* Redirect check button */}
         <div class="mt-1 mb-2 text-start">
           <input className="form-check-input" type="checkbox" value="" id="checkRedirect"
             defaultChecked={!redirect} onChange={() => { setRedirect(!redirect) }}
           />
-         
+
           {" "}
           <label className="form-check-label mr-2" for="checkRedirect">
-            No redirect to url 
+            No redirect to url
           </label>
         </div>
 
         {/* Resolve button */}
         <a className={"btn btn-lg btn-primary " + (valid ? '' : 'disabled')} href={jump_url} target="_blank" rel="noreferrer">Resolve ➜</a>
-      
+
       </div>
     </div>
   )
@@ -90,6 +117,7 @@ function SupportedPIDS() {
       <h5>Supported Pids</h5>
       <ul>
         <li>ARK</li>
+        <li>arxiv</li>
       </ul>
     </div>
   )
@@ -104,7 +132,7 @@ function App() {
       {/* Main Navigation bar */}
       <Navbar variant="light" expand="lg" className="main-nav shadow-sm">
         <Container>
-          
+
           {/* Branding logos */}
           <Navbar.Brand href="/">
             <img

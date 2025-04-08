ed9a66963cae1bcb1148d6bf5290282d8512eefe
evripant <evripidispantelaios@gmail.com>
2025-04-08 16:26:25 +0300

    refactor routers, extract route paths to constants

---
+++ src/server/endpoints/routes.ts
@@ -0,0 +1,27 @@
+const ROUTES = {
+  HOME: "/",
+  ABOUT: {
+    INTEROPERABILITY: "/about/interoperability",
+    ACCEPTABLE_USE: "/about/acceptable-use",
+    PRIVACY: "/about/privacy",
+    TERMS: "/about/terms",
+    DISCLAIMER: "/about/disclaimer",
+    COOKIES: "/about/cookies",
+  },
+  SUPPORTED_PIDS: "/supported-pids",
+  MANAGED_PIDS: {
+    ROOT: "/managed-pids",
+    ADD: "/managed-pids/add",
+    VIEW: "/managed-pids/view/:id",
+    EDIT: "/managed-pids/edit/:id",
+  },
+  USER_ROLE: {
+    ROOT: "/user-role",
+    REQUESTS: "/user-role-requests",
+    GUIDE: "/user-role-guide",
+    USERS_TABLE: "/users-table",
+  },
+  LOGOUT: "/logout",
+};
+
+export default ROUTES;
